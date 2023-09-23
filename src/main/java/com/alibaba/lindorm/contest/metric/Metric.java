49face92adb86fe3bb89945f52047c091c39c5c7
allenduke <AllenDuke@163.com>
2023-09-23 15:51:42 +0800

    metric

---
+++ src/main/java/com/alibaba/lindorm/contest/metric/Metric.java
@@ -0,0 +1,13 @@
+package com.alibaba.lindorm.contest.metric;
+
+public class Metric {
+
+    private String vin;
+
+    private int timeAggCnt;
+
+    /**
+     * time完整扫描批次
+     */
+    private int timeAggBatchCnt;
+}
